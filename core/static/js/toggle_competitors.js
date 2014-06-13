d720af8f3f9a99081d6e87212fba47d0d966ab05
Arimatea Neto <arineto30@gmail.com>
2014-06-13 16:03:36 -0400

    Competitors showing address

--- core/static/js/toggle_competitors.js
+++ core/static/js/toggle_competitors.js
@@ -2,6 +2,9 @@ var comp_hash = {};
 var comp_name;
 var comp_icon;
 
+var comp_window;
+
+
 function toggle_competitor(competitor, icon){
 	if (competitor.checked){
 		var request = {
@@ -28,17 +31,28 @@ function callback(results, status) {
   if (status == google.maps.places.PlacesServiceStatus.OK) {
     for (var i = 0; i < results.length; i++) {
       var place = results[i];
-      createMarker(place.name, place.geometry.location);
+      createMarker(place.name, place.geometry.location, place.formatted_address);
     }
   }
 }
 
-function createMarker(name, latlng){
-	marker = new google.maps.Marker({
+function createMarker(name, latlng, address){
+	var comp_marker;
+	comp_marker = new google.maps.Marker({
 		map: map,
 		position: latlng,
 		title: name,
 		icon: comp_icon
 	});
-	comp_hash[comp_name].push(marker);
+
+	google.maps.event.addListener(comp_marker, 'click', function () {
+		if(comp_window){
+			comp_window.setMap(null);
+		}
+		comp_window = new google.maps.InfoWindow({
+			content: address
+		})
+		comp_window.open(map, comp_marker);
+	});
+	comp_hash[comp_name].push(comp_marker);
 }
