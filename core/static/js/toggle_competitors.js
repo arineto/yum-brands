5368375ea6f1316f368971d5be7bc316fa4e5761
Arimatea Neto <arineto30@gmail.com>
2014-06-16 16:00:51 -0400

    Adding radar search to toggle_competitors

--- core/static/js/toggle_competitors.js
+++ core/static/js/toggle_competitors.js
@@ -3,21 +3,72 @@ var comp_name;
 var comp_icon;
 var comp_window;
 
+///////////////////////////// TEXT SEAERCH /////////////////////////////
+// function toggle_competitor(competitor, icon){
+// 	if (competitor.checked){
+// 		var request = {
+// 		    location: map.getCenter(),
+// 		    radius: '50000',
+// 		    query: competitor.name
+// 		};
+		
+// 		comp_icon = icon;
+// 		comp_name = competitor.name;
+// 		comp_hash[comp_name] = [];
 
+// 		service = new google.maps.places.PlacesService(map);
+// 		service.textSearch(request, callback);
+// 	}else{
+// 		array = comp_hash[competitor.name]
+// 		for (i=0; i<array.length; i++){
+// 			array[i].setMap(null);
+// 		}
+// 	}
+// }
+
+// function callback(results, status) {
+//   if (status == google.maps.places.PlacesServiceStatus.OK) {
+//     for (var i = 0; i < results.length; i++) {
+//       var place = results[i];
+//       createMarker(place.name, place.geometry.location, place.formatted_address);
+//     }
+//   }
+// }
+
+// function createMarker(name, latlng, address){
+// 	var comp_marker;
+// 	comp_marker = new google.maps.Marker({
+// 		map: map,
+// 		position: latlng,
+// 		title: name,
+// 		icon: comp_icon
+// 	});
+
+// 	google.maps.event.addListener(comp_marker, 'click', function () {
+// 		if(comp_window){
+// 			comp_window.setMap(null);
+// 		}
+// 		comp_window = new google.maps.InfoWindow({
+// 			content: address
+// 		})
+// 		comp_window.open(map, comp_marker);
+// 	});
+// 	comp_hash[comp_name].push(comp_marker);
+// }
+////////////////////////////////////////////////////////////////////////
+///////////////////////////// RADAR SEARCH /////////////////////////////
 function toggle_competitor(competitor, icon){
 	if (competitor.checked){
 		var request = {
-		    location: map.getCenter(),
-		    radius: '50000',
-		    query: competitor.name
+			bounds: map.getBounds(),
+			keyword: competitor.name
 		};
 		
 		comp_icon = icon;
 		comp_name = competitor.name;
 		comp_hash[comp_name] = [];
-
 		service = new google.maps.places.PlacesService(map);
-		service.textSearch(request, callback);
+		service.radarSearch(request, callback);
 	}else{
 		array = comp_hash[competitor.name]
 		for (i=0; i<array.length; i++){
@@ -27,31 +78,53 @@ function toggle_competitor(competitor, icon){
 }
 
 function callback(results, status) {
-  if (status == google.maps.places.PlacesServiceStatus.OK) {
+	if (status != google.maps.places.PlacesServiceStatus.OK) {
+	    alert(status);
+	    return;
+	}
     for (var i = 0; i < results.length; i++) {
-      var place = results[i];
-      createMarker(place.name, place.geometry.location, place.formatted_address);
-    }
-  }
+		var place = results[i];
+		// With detail
+		// var request = {
+		//   reference: place.reference
+		// };
+		// service.getDetails(request, callback2);
+		// Without detail
+		var comp_marker;
+		comp_marker = new google.maps.Marker({
+			map: map,
+			position: place.geometry.location,
+			// title: place.name,
+			icon: comp_icon
+		});
+		comp_hash[comp_name].push(comp_marker);
+  	}
 }
 
-function createMarker(name, latlng, address){
-	var comp_marker;
-	comp_marker = new google.maps.Marker({
-		map: map,
-		position: latlng,
-		title: name,
-		icon: comp_icon
-	});
+// function callback2(place, status) {
+//   if (status == google.maps.places.PlacesServiceStatus.OK) {
+//     createMarker(place);
+//   }
+// }
 
-	google.maps.event.addListener(comp_marker, 'click', function () {
-		if(comp_window){
-			comp_window.setMap(null);
-		}
-		comp_window = new google.maps.InfoWindow({
-			content: address
-		})
-		comp_window.open(map, comp_marker);
-	});
-	comp_hash[comp_name].push(comp_marker);
-}
+// function createMarker(place){
+// 	var comp_marker;
+// 	comp_marker = new google.maps.Marker({
+// 		map: map,
+// 		position: place.geometry.location,
+// 		title: place.name,
+// 		icon: comp_icon
+// 	});
+
+// 	google.maps.event.addListener(comp_marker, 'click', function () {
+// 		if(comp_window){
+// 			comp_window.setMap(null);
+// 		}
+// 		comp_window = new google.maps.InfoWindow({
+// 			content: "<p>"+place.name+"</p>"+"<p>"+place.formatted_address+"</p>"
+// 		})
+// 		comp_window.open(map, comp_marker);
+// 	});
+// 	comp_hash[comp_name].push(comp_marker);
+// }
+////////////////////////////////////////////////////////////////////////
